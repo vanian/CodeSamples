7fc824309c6e9db056dd89dbe7a74108dd09f010
Vani Ananthuni <vani.ananthuni@gmail.com>
2016-10-08 00:42:18 -0400

    Create typeofEverything.js

---
+++ typeofEverything.js
@@ -0,0 +1,17 @@
+//typeof everything - https://repl.it/DE92/0
+
+console.log(typeof "john" +'\n' + 
+    typeof 3.14 +'\n' + 
+    typeof NaN +'\n' + 
+    typeof false +'\n' + 
+    typeof [1,2,3,4] +'\n' + 
+    typeof {name:'john', age:34} +'\n' + 
+    typeof new Date() +'\n' + 
+    typeof function () {} +'\n' + 
+    typeof m +'\n' + 
+    typeof null +'\n' + 
+    typeof Object +'\n' + 
+    typeof undefined +'\n' +
+    typeof console +'\n' +
+    typeof console.log 
+    );
